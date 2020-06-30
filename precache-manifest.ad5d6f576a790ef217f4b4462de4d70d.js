0f4eb573136167b4830d31d9ea6add902a23d96f
shivampip <shivam301296@gmail.com>
2020-06-30 05:13:30 +0000

    Updates

---
+++ precache-manifest.ad5d6f576a790ef217f4b4462de4d70d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9654106927229ffb6e32b64fb55262",
+    "url": "/canvas/index.html"
+  },
+  {
+    "revision": "882470cf69125c20a1f1",
+    "url": "/canvas/static/css/main.94fbebae.chunk.css"
+  },
+  {
+    "revision": "f01efd8b2bf0525345b7",
+    "url": "/canvas/static/js/2.a8c8892e.chunk.js"
+  },
+  {
+    "revision": "bde11d1d662dddf94a5c81adf46cbedb",
+    "url": "/canvas/static/js/2.a8c8892e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882470cf69125c20a1f1",
+    "url": "/canvas/static/js/main.b98acf62.chunk.js"
+  },
+  {
+    "revision": "f8c99001642f62a88dd7",
+    "url": "/canvas/static/js/runtime-main.4a741586.js"
+  }
+]);
